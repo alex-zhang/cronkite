209271f1a10464800ae80ce75ccec76865f068e7
alex-zhang <gzane24@gmail.com>
2016-06-29 01:41:35 +0800

    add jobs

--- app/web/src/containers/App/index.jsx
+++ app/web/src/containers/App/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
 
 import Header from '../Header';
 import Footer from '../Footer';
@@ -7,12 +8,20 @@ import JobListPanel from '../JobListPanel';
 
 import styles from './s.styl';
 
+import JobList from '../JobList'
+
 class App extends React.Component {
 
   render() {
+    console.log(this.props.children + '---------');
     return (
       <div>
         <Header />
+        <div className={styles.content}>
+          <Paper style={{padding:"10px"}}>
+            <JobList />
+          </Paper>
+        </div>
         {this.props.children}
         <Footer />
       </div>
--- app/web/src/containers/JobList/index.jsx
+++ app/web/src/containers/JobList/index.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
-
+import Divider from 'material-ui/Divider';
+import RaisedButton from 'material-ui/RaisedButton';
+import {
+  Table,
+  TableBody,
+  TableFooter,
+  TableHeader,
+  TableHeaderColumn, TableRow, TableRowColumn}
+  from 'material-ui/Table';
 import styles from './s.styl';
 
 class JobsList extends React.Component {
 
   render() {
     return (
-      <p> asd</p>
+      <div>
+        <div style={{display:"flex", padding:"10px"}}>
+          <p>Jobs</p>
+          <RaisedButton label="Add New Job" />
+        </div>
+        <Divider />
+
+        <Table style={{height:'800px'}}>
+          <TableHeader>
+            <TableRow>
+              <TableRowColumn>Name</TableRowColumn>
+              <TableRowColumn>Executions</TableRowColumn>
+              <TableRowColumn>Host</TableRowColumn>
+              <TableRowColumn>Actions</TableRowColumn>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+
+          </TableBody>
+        </Table>
+
+      </div>
     );
   }
 }
